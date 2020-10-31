4833f98c5fd27fa6019a79725cb2adcee36a902b
John Elesho <eleshojohno@gmail.com>
2020-10-31 11:14:03 +0100

    Handle Create new User and and Get all User

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from enum import unique
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+import uuid
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 app = Flask(__name__)
-app.config['SECRET']= 'elesho'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todoapi.sqlite3'
+app.config['SECRET_KEY']= 'elesho'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todoapi.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 db = SQLAlchemy(app)
@@ -24,8 +25,55 @@ class Todo(db.Model):
     complete = db.Column(db.Boolean, nullable=False)
     user_id = db.Column(db.Integer, default=False, nullable=False)
 
+#Setting up the user routes that only admins who have logged in have access to
+@app.route('/user/', methods=['GET'])
+def get_all_users():
+    users = User.query.all()
+    output = []
+    for user in users:
+         user_data = {}
+         user_data['public_id'] = user.public_id
+         user_data['username'] = user.username
+         user_data['password'] = user.password
+         user_data['is_admin'] = user.is_admin
+         output.append(user_data)
 
+    return jsonify({"users": output})
 
+@app.route('/user/<user_id>', methods=["GET"])
+def get_one_user():
+    users = User.query.all()
+    output = []
+    for user in users:
+         user_data = {}
+         user_data['public_id'] = user.public_id
+         user_data['username'] = user.username
+         user_data['password'] = user.password
+         user_data['is_admin'] = user.is_admin
+         output.append(user_data)
+
+    return jsonify({"users": output})
+
+@app.route('/user/', methods=['POST'])
+def create_user():
+    data = request.get_json()
+    hashed_password = generate_password_hash(data['password'], method='sha256')
+    new_user_public_id = str(uuid.uuid4())
+    new_user_name = data['username']
+   
+    new_user = User(public_id = new_user_public_id , username = new_user_name, password=hashed_password, is_admin=False )
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({'Message': 'New User Created'})
+
+@app.route('/user/<user_id>', methods=["PUT"])
+def promote_user():
+    return ""
+
+@app.route('/user/<user_id>', methods=["DELETE"])
+def delete_user():
+    return ""
 
 if __name__ == "__main__":
+    db.create_all()
     app.run(debug=True)
